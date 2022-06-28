f499708eb30551845edac4c38df29bfa05da3247
NineTrier <63592407+NineTrier@users.noreply.github.com>
2022-06-28 13:59:31 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from docx.shared import Inches, Pt
 
 doc = Document('Решение.docx')
 
-for p in doc.paragraphs:
-    if " N " in p.text:
-        text = p.text.replace(' N ',' № ')
-        style = p.style
-        p.text = text
-        p.style = style
-    if '"' in p.text:
-        text = p.text.replace('"', '')
-        style = p.style
-        p.text = text
-        p.style = style
 
+def ReplaceN():
+    for p in doc.paragraphs:
+        if " N " in p.text:
+            text = p.text.replace(' N ',' № ')
+            style = p.style
+            p.text = text
+            p.style = style
+        if ' "' in p.text and '" ' in p.text:
+            text = p.text.replace(' "',' «')
+            text1 = text.replace('" ','» ')
+            style = p.style
+            p.text = text1
+            p.style = style
+        if '“' in p.text and '”' in p.text:
+            text = p.text.replace('“',' «')
+            text1 = text.replace('”','» ')
+            style = p.style
+            p.text = text1
+            p.style = style
     doc.save('test.docx')
+
+ReplaceN()
